bcd0e0a3203ff93d44f9e0fc58d144014475b83c
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-01-11 19:53:07 +0100

    Copied in styling for all sections

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,7 @@ import Header from "./Header";
 import PlayerSelect from "./PlayerSelect";
 import PartyList from "./PartyList";
 import MonsterToggle from "./MonsterToggle";
+import PlayerSummary from "./PlayerSummary";
 
-export { Header, PlayerSelect, PartyList, MonsterToggle };
+
+export { Header, PlayerSelect, PartyList, MonsterToggle, PlayerSummary };
