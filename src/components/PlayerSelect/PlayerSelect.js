81ee4cfbd054e3f87b71433983413345bc9c0387
Ben Ciantar <benjamin.ciantar@hyperisland.se>
2022-01-12 17:04:27 +0100

    Party size select correctly updating state and calling next function

--- src/components/PlayerSelect/PlayerSelect.js
+++ src/components/PlayerSelect/PlayerSelect.js
@@ -1,16 +1,31 @@
+import React, { useState, useEffect } from "react";
+
 function PlayerSelect() {
+  const [numPlayers, setNumPlayers] = useState(1);
+
+  const [handleChange] = useState(() => {
+    return () => {
+      setNumPlayers(document.getElementById('number-of-players').value);
+    };
+  });
+
+  useEffect(() => {
+    console.log(numPlayers);
+  }, [numPlayers])
+  
+
   return (
     <div id="player-select">
         <label htmlFor="number-of-players">Party Size:</label>
-        <select name="players" id="number-of-players">
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-            <option value="4">Four</option>
-            <option value="5">Five</option>
-            <option value="6">Six</option>
-            <option value="7">Seven</option>
-            <option value="8">Eight</option>
+        <select name="players" id="number-of-players" onChange={handleChange}>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+          <option value="4">Four</option>
+          <option value="5">Five</option>
+          <option value="6">Six</option>
+          <option value="7">Seven</option>
+          <option value="8">Eight</option>
         </select>
     </div>
   );
